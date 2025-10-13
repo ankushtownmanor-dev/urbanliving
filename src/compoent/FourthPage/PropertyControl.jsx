917c7c3944359ba8257b2f017154a72e08868adc
Sunny <sunnysingh29122001@gmail.com>
2025-10-13 16:14:01 +0530

    new changes

--- src/compoent/FourthPage/PropertyControl.jsx
+++ src/compoent/FourthPage/PropertyControl.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import "./PropertyControl.css";
- // Replace with your actual image path
 
 const PropertyControl = () => {
   return (
     <div className="property-control-container">
       <div className="property-text-section">
-        <h2>Complete Control at Your Figertips</h2>
+        <h2>Complete Control at Your Fingertips</h2>
         <p>
           Want full control over your property listing? Ovika gives you the
-          freedom to upload <br/> and manage property on your own terms. Easily add
-          photos, set rent details, write <br/>  descriptions, and publish property
-          instantly on our platform. With our simple and <br/>  user-friendly
-          interface, you can reach potential tenants, directly track enquiries,
-          and <br/>  update details anytime you want. It is the perfect choice for
-          property owners so  <br/>  proper independent and hands-on management while
-          benefiting from Ovika’s trusted <br/>  platform and audience reach.
+          freedom to upload <br /> and manage property on your own terms. Easily
+          add photos, set rent details, write <br /> descriptions, and publish
+          property instantly on our platform. With our simple and <br /> 
+          user-friendly interface, you can reach potential tenants, directly
+          track enquiries, and <br /> update details anytime you want. It is the
+          perfect choice for property owners who <br /> prefer independent and
+          hands-on management while benefiting from Ovika’s trusted <br /> 
+          platform and audience reach.
         </p>
       </div>
       <div className="property-image-section">
-        <img src='/public/image 219.png' alt="Property" />
+        <img src="/public/image 219.png" alt="Property" />
       </div>
     </div>
   );
--- src/compoent/FourthPage/PropertyShare.jsx
+++ src/compoent/FourthPage/PropertyShare.jsx
@@ -6,15 +6,16 @@ function FourthMain() {
     <div className="property-share-container">
       <div className="property-share-overlay">
         <div className="property-share-content">
-          <div className='property-share-header'>
+          <div className="property-share-header">
             <p className="property-share-title">
-              Share your property - we renovate <br/> & share profit
+              Share your property - we renovate <br /> & share profit
             </p>
           </div>
           <p className="property-share-text">
             Take full control of your rental listing.
-            With Ovika, you can easily<br/> find qualified tenants,
-            manage inquiries, and publish your <br/> property in minutes.
+            With Ovika, you can easily <br />
+            find qualified tenants, manage inquiries, and publish your <br />
+            property in minutes.
           </p>
           <button className="property-share-button">
             Get Started For Free
