6a9c91cd3d6c23352d67720e75d88551c041419e
Sunny <sunnysingh29122001@gmail.com>
2025-11-11 12:34:43 +0530

    button

--- src/compoent/Homepage/HoomieFooter.jsx
+++ src/compoent/Homepage/HoomieFooter.jsx
@@ -1232,7 +1232,7 @@ const HoomieFooter = () => {
                       alignItems: "center",
                       justifyContent: "center",
                       borderRadius: "50px",
-                      marginRight: "-2px",
+                      marginRight: "-12px",
                       transition: "0.3s ease",
                     }}
                   >
