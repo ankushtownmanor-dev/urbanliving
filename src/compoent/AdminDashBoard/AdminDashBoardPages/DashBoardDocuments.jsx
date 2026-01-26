bc9d67d6ef804d01ce945f5fd2b29dd114b67e6e
Sunny <sunnysingh29122001@gmail.com>
2026-01-26 10:38:30 +0530

    new changes

--- src/compoent/AdminDashBoard/AdminDashBoardPages/DashBoardDocuments.jsx
+++ src/compoent/AdminDashBoard/AdminDashBoardPages/DashBoardDocuments.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './DashboadDocument.css';
 
- const DashBoardDocuments = () => {
+const DashBoardDocuments = () => {
   return (
-    <div>DashBoardDocuments</div>
-  )
-}
-export default DashBoardDocuments
+    <div className="documents-container">
+      <div className="documents-content">
+        <div className="icon">📄</div>
+        <h2>Documents</h2>
+        <p>This provision is coming soon</p>
+      </div>
+    </div>
+  );
+};
+
+export default DashBoardDocuments;
