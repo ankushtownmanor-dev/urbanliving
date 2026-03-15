af9d266370003f0976a9607cb0992971a456655c
Sunny <sunnysingh29122001@gmail.com>
2026-03-15 15:29:43 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,25 @@
 
 
 
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import ReactGA from "react-ga4";
+// import OneSignal from "react-onesignal";
+
+// ReactGA.initialize("G-FPLVT7TZ2L");
+// OneSignal.init({
+//   appId: "94be9ed7-5b37-41b7-bcf3-0903cce5cf2c",
+// });
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -22,12 +41,20 @@ import ReactGA from "react-ga4";
 import OneSignal from "react-onesignal";
 
 ReactGA.initialize("G-FPLVT7TZ2L");
-OneSignal.init({
-  appId: "94be9ed7-5b37-41b7-bcf3-0903cce5cf2c",
-});
+
+async function initOneSignal() {
+  await OneSignal.init({
+    appId: "94be9ed7-5b37-41b7-bcf3-0903cce5cf2c",
+    notifyButton: {
+      enable: true,
+    },
+  });
+}
+
+initOneSignal();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
